aea343bec2aae2109b463dfd39e82bddee7add3d
tomonobu <bokotomo@me.com>
2016-12-05 09:03:29 +0900

    edit readme and suport webmdparser urltext

--- text.md
+++ text.md
@@ -4,3 +4,7 @@ URLに.mdファイルを指定しても整形された状態で見れます。
 # Example
 http://domain.com/text.md
 このURLは通常ならtxtファイルとして出力されますが、WebMdParserを使うとmdファイルとして見ることができます。
+
+# title
+## title2
+### title3
--- webmdparser/md_parser_controller.php
+++ webmdparser/md_parser_controller.php
@@ -16,17 +16,59 @@ class MdParserController
     echo file_get_contents("footer.txt");
   }
 
+  private function getShapeNum($textArray, $textMaxNum, $index){
+    $titleNum = 1;
+    for($j = $index + 1;$j<$textMaxNum;$j++){
+      if($textArray[$j] == "#"){
+        $titleNum++;
+      }else{
+        break;
+      }
+    }
+    return $titleNum;
+  }
+
   private function parseMdText($textArray){
     $convertedText = "";
     $textMaxNum = count($textArray);
     for($i = 0;$i<$textMaxNum;$i++){
       if($textArray[$i] == "\n"){
         $convertedText .= "<br>";
+      }else if($textArray[$i] == "h" && $textArray[$i + 1] == "t" && $textArray[$i + 2] == "t" && $textArray[$i + 3] == "p"){
+        $urlText = "";
+        for($j = $i + 4;$j<$textMaxNum;$j++){
+          if($textArray[$j] == "\n" || $textArray[$j] == " " || $textArray[$j] == "　"){
+            $i = $j;
+            break;
+          }else if($j == $textMaxNum - 1){
+            $urlText .= $textArray[$j];
+            $i = $j;
+            break;
+          }else{
+            $urlText .= $textArray[$j];
+          }
+        }
+        $convertedText .= "<a href='http{$urlText}' target='_blank'>http{$urlText}";
+        $convertedText .= "</a>";
+        if($textArray[$i] == "\n"){
+          $convertedText .= "<br>";
+        }else if($i == $textMaxNum - 1){
+          //none
+        }else{
+          $convertedText .= $textArray[$i];
+        }
       }else if($textArray[$i] == "#"){
+        //h1
+        $titleNum = $this->getShapeNum($textArray, $textMaxNum, $i);
         if($i==0){
-          $convertedText .= "<div class='title1'>";
-          for($j = $i + 1;$j<$textMaxNum;$j++){
+          $convertedText .= "<div class='title{$titleNum}'>";
+          for($j = $i + $titleNum;$j<$textMaxNum;$j++){
             if($textArray[$j] == "\n"){
+              $convertedText .= "</div>";
+              $i = $j;
+              break;
+            }else if($j == $textMaxNum - 1){
+              $convertedText .= $textArray[$j];
               $convertedText .= "</div>";
               $i = $j;
               break;
@@ -36,9 +78,14 @@ class MdParserController
           }
         }else{
           if($textArray[$i - 1] == "\n"){
-            $convertedText .= "<div class='title1'>";
-            for($j = $i + 1;$j<$textMaxNum;$j++){
+            $convertedText .= "<div class='title{$titleNum}'>";
+            for($j = $i + $titleNum;$j<$textMaxNum;$j++){
               if($textArray[$j] == "\n"){
+                $convertedText .= "</div>";
+                $i = $j;
+                break;
+              }else if($j == $textMaxNum - 1){
+                $convertedText .= $textArray[$j];
                 $convertedText .= "</div>";
                 $i = $j;
                 break;
